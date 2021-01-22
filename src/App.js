09cc627a98207022feacb62bddb16c21d0f4c1d8
Nejc <nejcrogelsek0@gmail.com>
2021-01-22 17:21:17 +0100

    header HTML structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <Header />
+    {/* items => snapping */}
+    {/* items => snapping */}
+    {/* items => snapping */}
+    {/* items => snapping */}
+  </div>;
 };
 
 export default App;
